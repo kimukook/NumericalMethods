6cd45a36adb4013c86eaad34c019d82b3a48dd57
Muhan Zhao <muz021@ucsd.edu>
2023-09-21 10:36:55 -0700

    rk45 fixed;

--- rk45.m
+++ rk45.m
@@ -1,15 +1,29 @@
 function [x, t] = rk45(f, x, t, simParam)
-% rk45 time marching code, slightly modified from NR P292.
+% This script implements the classic four-stage, fifth-order accurate Rungze 
+% Kutta scheme for time advancment of ODE system, dx/dt = f(x,t), with a slight
+% modification of Prof. Thomas Bewley's Numerical Renaissance P292. This
+% script is suitable to march the state both forward and backward in time.
+% For backward marching, simply set t to be negative real number.
 % Input:
-% feval: the function handle, x'= f(x)
-% x: the initial state
-% t: the initial time
-% h: the initla time step
-% p: the time-dependent (or, state-depenedent) parameter for feval
+%       f         :  function handle, x'= f(x)
+%       x         :  initial state
+%       t         :  initial time
+%       simParam.h:  time step for marching
+%       simParam.p:  time(or, state)-dependent parameter of the RHS of ODE
 % 
 % Return:
-% x: final state
-% t: final time
+%       x         :  final state
+%       t         :  final time
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Author       :    Muhan Zhao and Thomas Bewley
+% Institute    :    Mechanical and Aerospace Engineering, UC San Diego
+% Date         :    Feb., 2023
+
+
+%%%%%%%%%%
+% TODO record x for all time instants b/w t and T
+%%%%%%%%%%
+
 if nargin < 4 || ~isfield(simParam, 'h')
     simParam.h = t+sign(t)*1e-10;
 end
@@ -24,19 +38,28 @@ end
 
 H = simParam.h; h = H/2; T = simParam.T;
 while abs(t) < abs(T)
+    % calculate X with one RK4 full timestep H
     f1 = feval(f, x); 
     f2 = feval(f, x + H * f1/2); 
     f3 = feval(f, x + H * f2/2);
     f4 = feval(f, x + H * f3);
     X = x + H * (f1/6 + (f2 + f3)/3 + f4/6);
 
+    % calculate X with two RK4 sub timesteps h=H/2
     f1 = feval(f, x); 
     f2 = feval(f, x + h * f1/2);
     f3 = feval(f, x + h * f2/2);
     f4 = feval(f, x + h * f3);
     x = x + h * (f1/6 + (f2 + f3)/3 + f4/6);
-    delta = norm(x - X, 1) / 15;
+    f1 = feval(f, x); 
+    f2 = feval(f, x + h * f1/2);
+    f3 = feval(f, x + h * f2/2);
+    f4 = feval(f, x + h * f3);
+    x = x + h * (f1/6 + (f2 + f3)/3 + f4/6);
 
+    % estimate of error
+    delta = norm(x - X, 1) / 15;
+    % determine the new x based on adaptive rk45
     x = (x * 16 - X) / 15; t = t + H;
     H = min(abs(H)*(abs(H)*simParam.epsOverT / delta)^(1/4), abs(simParam.T-t));
 end
--- twoD_nabla_uniform.m
+++ twoD_nabla_uniform.m
@@ -1,10 +1,26 @@
 function [NABx, NABy] = twoD_nabla_uniform(Nx, Ny, delta)
-% Compute the nabla operator for x and y dimension, using second order
-% central difference in the interior of the domain, and second order
-% forward/backward difference at the boundary of the domain.
+% Compute the nabla operator for x and y axis assuming a uniform spacing.
+% The first order derivative of the interior of the domain is approximated
+% by the second order central differencing scheme, while the second order
+% forward/backward difference is applied at the boundary of the domain.
 % 
-% This script aims to accelerate the computation via the avoidance of the 
-% double for loop. Rather than that, the script uses kronecker matrix product.
+% Both nabla operators are "row-wise", namely NAB_(:,j) denotes the value
+% of discretized x-axis associated with y_j. This convention is compatible
+% with the common understanding of the theoretical development in numerical 
+% analysis.
+% 
+% User might define the boundary condition at their own interest with a
+% slight modification in this script.
+% 
+% Input:
+%       Nx      : The number of discretized points in x
+%       Ny      : The number of discretized points in y
+%       delta   : The step size of each axis
+% Example:
+% [NABx, NABy] = twoD_nabla_uniform(64, 64, .01);
+% 
+% This script aims to accelerate the computation by the substituion of the 
+% double for loop with the kronecker matrix product.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Author       :    Muhan Zhao
 % Institute    :    Flow & Control Lab, Mechanical and Aerospace Engineering, UC San Diego
@@ -17,10 +33,10 @@ Lx = spdiags([-ex/2, zeros(Nx, 1), ex/2], [-1, 0, 1], Nx, Nx);
 
 % define boundary condition for x-axis
 
-% 2nd forward difference
+% 2nd forward difference for west boundary
 temp = zeros(1, Nx); temp(1) = -3/2; temp(2) = 2; temp(3) = -1/2;
 Lx(1, :) = temp;
-% 2nd backward difference
+% 2nd backward difference for east boundary
 temp = zeros(1, Nx); temp(end) = 3/2; temp(end-1) = -2; temp(end-2) = 1/2;
 Lx(end, :) = temp;
 
@@ -35,11 +51,11 @@ NABy = kron(Ly, eye(Nx));
 
 % define boundary condition for y-axis
 
-    % 2nd order forward difference
+% 2nd order forward difference for south boundary
 temp = cat(2, -3/2*eye(Ny), 2*eye(Ny), -1/2*eye(Ny));
 NABy(1:Ny, 1:3*Ny) = temp;
 
-% 2nd order backward difference
+% 2nd order backward difference for north boundary
 temp = cat(2, 1/2*eye(Ny), -2*eye(Ny), 3/2*eye(Ny));
 NABy(end-Ny+1:end, end-3*Ny+1:end) = temp;
 
